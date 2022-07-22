70145e2440eee40c07149550aba2cf11a51969de
tonii092 <tonibusatta@gmail.com>
2022-07-22 22:56:23 +0400

    update product page

--- src/components/Common.js
+++ src/components/Common.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 // Color Palette ///////////////////////////////////////////////////////////////
 
 export const mainColor = "#FAF7EE";
@@ -17,3 +19,203 @@ export const muliBold = "Muli Bold";
 export const playfairDisplayMedium = "PlayfairDisplay Medium";
 export const playfairDisplayRegular = "PlayfairDisplay Regular";
 export const baliviaRegular = "Balivia Regular";
+
+// Components  ////////////////////////////////////////////////////////////////
+
+export const Item = styled.div`
+  height: 80vh;
+  width: 100%;
+  @media (max-width: 350px) {
+    height: 70vh;
+  }
+`;
+export const Name = styled.div`
+  height: 10%;
+  width: 100%;
+`;
+export const Overview = styled.div`
+  height: 70%;
+  width: 100%;
+`;
+export const Description = styled.div`
+  height: auto;
+  width: 100%;
+`;
+
+export const Layout = styled.div`
+  height: 100%;
+  width: 100%;
+  padding: 0 1rem;
+`;
+
+export const Title = styled.h1`
+  position: relative;
+  font-family: ${baliviaRegular};
+  font-weight: 700;
+  font-size: 2.5rem;
+  letter-spacing: 0.03em;
+  text-transform: uppercase;
+  text-align: left;
+  max-width: 250px;
+  @media (max-width: 350px) {
+    font-size: 2rem;
+  }
+
+  &::before {
+    position: absolute;
+    content: "";
+    bottom: -1rem;
+    left: 5%;
+    width: 112px;
+    height: 3px;
+    background: #000;
+    z-index: 10;
+    @media (max-width: 350px) {
+      width: 85px;
+    }
+  }
+
+  &.airfreshener_line::before {
+    background-color: #bc89fd;
+  }
+  &.antibacterial_line::before {
+    background-color: #b7e2fa;
+  }
+  &.handwash_line::before {
+    background-color: #feda57;
+  }
+  &.perfume_line::before {
+    background-color: #fbdce2;
+  }
+  &.coffee_line::before {
+    background-color: #af7f61;
+  }
+  &.food_line::before {
+    background-color: #ff6262;
+  }
+  &.showergel_line::before {
+    background-color: #f76b84;
+  }
+  &.bodymist_line::before {
+    background-color: #2e2b27;
+  }
+`;
+export const NameContent = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  margin: 0 3rem 0 0;
+
+  & span {
+    height: auto;
+    width: auto;
+    position: relative;
+    margin-left: 3rem;
+  }
+
+  & span a {
+    position: relative;
+    color: #222;
+    font-family: ${muliBold};
+    font-size: 1.3rem;
+    z-index: 1;
+    text-transform: uppercase;
+    white-space: nowrap;
+    transition: all 0.4s ease-out;
+    @media (max-width: 350px) {
+      font-size: 1rem;
+    }
+
+    &:before {
+      content: "";
+      display: block;
+      position: relative;
+      width: 0;
+      top: 2rem;
+      border-bottom: 2px solid #000;
+      transition: width 0.4s ease-out;
+      z-index: 2;
+    }
+  }
+`;
+
+export const Svg = styled.svg`
+  display: none;
+  height: auto;
+  width: auto;
+  min-height: 80px;
+  min-width: 120px;
+  position: absolute;
+  bottom: 0;
+  left: 0%;
+  transform: translate(-20%, 40%);
+  @media (min-width: 850px) {
+    display: block;
+  }
+
+  &.airfreshener_color path {
+    fill: #bc89fd;
+  }
+  &.antibacterial_color path {
+    fill: #b7e2fa;
+  }
+  &.handwash_color path {
+    fill: #feda57;
+  }
+  &.perfume_color path {
+    fill: #fbdce2;
+  }
+  &.coffee_color path {
+    fill: #af7f61;
+  }
+  &.food_color path {
+    fill: #ff6262;
+  }
+  &.showergel_color path {
+    fill: #f76b84;
+  }
+  &.bodymist_color path {
+    fill: #2e2b27;
+  }
+`;
+
+export const OverviewContent = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const Image = styled.img`
+  height: auto;
+  width: 80%;
+  max-width: 550px;
+  @media (min-width: 650px) {
+    width: 70%;
+  }
+  @media (min-width: 850px) {
+    width: 60%;
+  }
+`;
+
+export const DescriptionContent = styled.div`
+  height: 60%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+export const P = styled.p`
+  font-family: ${muliRegular};
+  font-size: 1.4rem;
+  font-weight: 400;
+  line-height: 2.3rem;
+  text-align: center;
+  color: #222;
+  @media (max-width: 350px) {
+    font-size: 1.2rem;
+  }
+`;
--- src/components/pageContent/Products.js
+++ src/components/pageContent/Products.js
@@ -11,80 +11,32 @@ import ItemPerfume from "./ItemsProducts/ItemPerfume";
 import ItemAirFreshener from "./ItemsProducts/ItemAirFreshener";
 
 const Container = styled.section`
-  height: 500vh;
+  height: auto;
   width: 100%;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10vh;
 `;
 
 function Products() {
   return (
     <Container>
+      <Wrapper>
 
-      <div id="produits" className="produits_accueil">
-        <div className="products_box">
-          <div className="products_box_bottom">
-            <div className="products_bottom_content">
-              <div className="products_wrapper">
-                <div className="products_grid accueil_padding">
+        <ItemAntibacterial />
+        <ItemHandwash />
+        <ItemShowerGel />
+        <ItemPerfume />
+        <ItemBodyMist />
+        <ItemAirFreshener />
+        <ItemCoffee />
+        <ItemFood />
 
-                    <ItemAntibacterial />
-
-                    <ItemHandwash />
-
-                    <ItemBodyMist />
-
-                    <ItemShowerGel />
-
-                    <ItemPerfume />
-
-                    <ItemCoffee />
-
-                    <ItemAirFreshener />
-
-                    <ItemFood />
-
-                  <div className="brands">
-                    <div className="brands_top">
-                      <div className="products_tittle">
-                        <span>
-                          <h1>Nos Marques</h1>
-                          <svg
-                            width="272"
-                            height="32"
-                            viewBox="0 0 272 32"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M1.60044 25.4382C30.7327 7.85973 63.4391 7.49884 141.26 18.1553"
-                              stroke="#FF9B71"
-                              strokeWidth="5"
-                            />
-                            <path
-                              d="M128.819 13.4999C134.718 15.0127 146.945 16.3379 162.02 17.2055C177.017 18.0685 194.612 18.468 211.115 18.1876C227.649 17.9068 242.97 16.945 253.472 15.1165C258.786 14.1912 262.575 13.0895 264.674 11.9063C265.74 11.3056 266.014 10.879 266.057 10.7812C266.063 10.7678 266.059 10.7719 266.057 10.7893C266.054 10.8072 266.056 10.8192 266.056 10.8183C266.055 10.8162 266.039 10.6982 265.877 10.4524L270.054 7.70378C271.047 9.21272 271.414 11.005 270.641 12.7791C269.957 14.3477 268.561 15.4549 267.129 16.2621C264.233 17.8946 259.713 19.105 254.33 20.0424C243.435 21.9393 227.801 22.9049 211.2 23.1869C194.569 23.4694 176.85 23.0672 161.733 22.1972C146.695 21.3318 134.009 19.9927 127.577 18.3431L128.819 13.4999Z"
-                              fill="#FF9B71"
-                            />
-                          </svg>
-                        </span>
-                      </div>
-                    </div>
-
-                    <div className="brands_bottom">
-                      <div className="brands_bottom_content">
-                        <div className="brands_grid">
-                          <div className="brands_image tukas"></div>
-                          <div className="brands_image nestle"></div>
-                          <div className="brands_image ultracompact"></div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
+      </Wrapper>
     </Container>
   );
 }
