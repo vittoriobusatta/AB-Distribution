6beb43979fa2ad646ffc6e9ee2804787e6fa8381
Vittorio <tonibusatta@gmail.com>
2022-07-27 17:26:42 +0400

    fix bug landing title

--- src/components/Common.js
+++ src/components/Common.js
@@ -126,6 +126,9 @@ export const LandingTitle = styled.h1`
   width: fit-content;
   margin: 100px auto;
 
+  @media screen and (max-width: 500px) {
+    font-size: 65px;
+  }
   @media screen and (max-width: 400px) {
     font-size: 50px;
   }
